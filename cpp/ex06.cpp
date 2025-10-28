88766c3631cce88c764788334a20107f3671dbde
adamcatching <adamcatching@gmail.com>
2025-10-28 11:39:56 -0400

    Fixed long strings, messy

--- cpp/ex06.cpp
+++ cpp/ex06.cpp
@@ -7,15 +7,15 @@ int main() {
     float height = 6.25f;
     string name = "Zed";
     char initial = 'A';
-    string last = 'Shaw';
+    auto last = "Shaw";
     long int big_negative = -123'456'789;
-    unsigned long long = lots_of_pi = 3'141'592'653'589'793'238;
+    unsigned long long lots_of_pi = 3'141'592'653'589'793'238;
 
     cout << "I am " << age << " years old.\n";
-    cout << "I am also " << height << " length of units tall.\n";
+    cout << "I am also " << height << " length units tall.\n";
     cout << "My name is " << name << " " << initial << " " << last << endl;
     cout << "Big negative number " << big_negative << endl;
-    cout << "Digitis of Pi " << lots_of_pi << endl;
-
+    cout << "Digits of Pi " << lots_of_pi << endl;
+    
     return 0;
 }
