5b3e1d33a31d24ad9e58abc49b0879a45aa86f2d
adamcatching <adamcatching@gmail.com>
2025-10-28 11:38:23 -0400

    Add current broken code

---
+++ cpp/ex06.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int age = 50;
+    float height = 6.25f;
+    string name = "Zed";
+    char initial = 'A';
+    string last = 'Shaw';
+    long int big_negative = -123'456'789;
+    unsigned long long = lots_of_pi = 3'141'592'653'589'793'238;
+
+    cout << "I am " << age << " years old.\n";
+    cout << "I am also " << height << " length of units tall.\n";
+    cout << "My name is " << name << " " << initial << " " << last << endl;
+    cout << "Big negative number " << big_negative << endl;
+    cout << "Digitis of Pi " << lots_of_pi << endl;
+
+    return 0;
+}
