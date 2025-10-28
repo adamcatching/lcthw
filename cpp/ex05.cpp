88693106a78877d65caff2f76936b3ad2479ec8d
adamcatching <adamcatching@gmail.com>
2025-10-28 11:06:42 -0400

    Chapter 5: familiarity with namespaces

--- cpp/ex05.cpp
+++ cpp/ex05.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
 int main() {
+    std::cout << "This is printed.\n";
+    std::cerr << "This writes to the error output.\n";
+    
+    // notice the \t  at the start
+    std::cout << "\tThis is tabbed in.\n";
     return 0;
 }
