5dbf9c4f2f3675c6db5e80b9e1f0c0a29c512f3f
adamcatching <adamcatching@gmail.com>
2025-10-31 10:30:55 -0400

    Chapter 8 challenge 1

--- cpp/ex08.cpp
+++ cpp/ex08.cpp
@@ -7,11 +7,20 @@ using namespace std;
 int main() {
     // CHALLENGE 1 HERE
 
-    string from_number = std::to_string(123456);
-    string from_float = std::to_string(1.1000f);
+    string from_number = to_string(123456);
+    string from_float = to_string(1.1000f);
+    int a = stoi("1234");
+    long b = stol("1234");
+    long long c = stoll("1234");
+    unsigned long d = stoul("1234");
+    unsigned long long e = stoull("1234");
+    float f = stof("1234");
+    double g = stod("1234");
+    long double h = stold("1234");
 
     ostringstream msg;
 
+    //msg << "Test numbers: " << a << b << c << d << e << f << endl;
     // CHALLENGE 2 HERE
 
     msg << "From number " << from_number << endl;
